f4c97ec799da8e08d3f5392ede110d737fc43a18
Abbishek <krishabbi17@gmail.com>
2016-09-15 09:00:49 +0530

    update projects

--- js/app.js
+++ js/app.js
@@ -3,7 +3,7 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic'])
+var app = angular.module('starter', ['ionic'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
@@ -21,4 +21,57 @@ angular.module('starter', ['ionic'])
       StatusBar.styleDefault();
     }
   });
+
+  
+
+})
+
+    .config(function ($stateProvider, $urlRouterProvider) {
+        $stateProvider.state('index', {
+            url: '/',
+            templateUrl: 'index.html',
+            controller: 'MainController'
+        });
+
+        $urlRouterProvider.otherwise("/");
+    });
+
+app.controller("MainController", ['$scope', function ($scope) {
+    $scope.test = "";
+}]);
+
+// angular.module('ionicApp', ['ionic'])
+
+app.controller('Ctrl', function ($scope) {
+    $scope.data = {};
+    $scope.data.bgColors = [];
+    $scope.data.currentPage = 0;
+
+    for (var i = 0; i < 10; i++) {
+        $scope.data.bgColors.push("bgColor_" + i);
+    }
+
+    var setupSlider = function () {
+        //some options to pass to our slider
+        $scope.data.sliderOptions = {
+            initialSlide: 0,
+            direction: 'horizontal', //or vertical
+            speed: 300 //0.3s transition
+        };
+
+        //create delegate reference to link with slider
+        $scope.data.sliderDelegate = null;
+
+        //watch our sliderDelegate reference, and use it when it becomes available
+        $scope.$watch('data.sliderDelegate', function (newVal, oldVal) {
+            if (newVal != null) {
+                $scope.data.sliderDelegate.on('slideChangeEnd', function () {
+                    $scope.data.currentPage = $scope.data.sliderDelegate.activeIndex;
+                    //use $scope.$apply() to refresh any content external to the slider
+                    $scope.$apply();
+                });
+            }
+        });
+    };
+    setupSlider();
 })
